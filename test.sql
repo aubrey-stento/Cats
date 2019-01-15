bc6cfb576078ce431def94ebdbd68d9c88ec49bf
Aubrey Stento <aubreystento@gmail.com>
2019-01-15 12:03:46 -0700

    add sql file

---
+++ test.sql
@@ -0,0 +1,12 @@
+CREATE TABLE cats(
+    id int NOT NULL AUTO_INCREMENT
+    catName VARCHAR(255),
+    Age int
+    PRIMARY KEY (id)
+)
+
+-- INSERT INTO cats(CatName, Age) VALUES("King Arpurr", 3);
+
+-- SELECT * FROM cats;
+
+DROP TABLE cats;
